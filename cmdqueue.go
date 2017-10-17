5c922929114d0ddc7857ede0aa0483e7f9a021b7
Alexsoul133 <alexsoul133@gmail.com>
2017-10-17 18:12:19 +1000

    sunc

--- cmdqueue.go
+++ cmdqueue.go
@@ -8,10 +8,12 @@ import (
 
 var run [][]string
 
+//Newcmd - push command to stack run
 func Newcmd(s []string) {
 	run = append(run, s)
 }
 
+//Start - run stack of cmd
 func Start() {
 	for i := range run {
 		cmd := exec.Command("cmd", "/K", strings.Join(run[i], " "))
